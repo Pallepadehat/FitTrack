50e9a1a87ef375e700f20908d016d5f73a934021
Pallepadehat <patrick.jakobsen@icloud.com>
2026-01-04 16:39:14 +0100

    feat: create WorkoutplanTab and integrate it into the RootView.

---
+++ FitTrack/Tabs/Workout/WorkoutplanTab.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutplanTab.swift
+//  FitTrack
+//
+//  Created by Patrick Jakobsen on 04/01/2026.
+//
+
+import SwiftUI
+
+struct WorkoutplanTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutplanTab()
+}
