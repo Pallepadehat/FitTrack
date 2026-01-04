0acf623492a2ac01bd9661cccc2611f1b1dd4e2e
Pallepadehat <patrick.jakobsen@icloud.com>
2026-01-04 15:19:42 +0100

    Initial Commit

---
+++ FitTrack/FitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrackApp.swift
+//  FitTrack
+//
+//  Created by Patrick Jakobsen on 04/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
